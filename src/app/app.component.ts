e5ddbea4e872b8b94b404bafb83bb5527555ae4e
morgan <pttrsn.mrgn@gmail.com>
2022-10-27 20:59:59 -0500

    completed lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './MenuItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'menu';
+
+  menuItems: MenuItem[] = [
+    {
+      item: 'Chicken Fingers',
+      category: 'dinner',
+      price: "11.99"
+    },
+    {
+      item: 'Pizza',
+      category: 'dinner',
+      price: "11.99",
+    },
+    {
+      item: 'Wings',
+      category: 'sides',
+      price: "8.99",
+    },
+    {
+      item: 'Breadsticks',
+      category: 'sides',
+      price: "4.99",
+    },
+    {
+      item: 'Ceasar Salad',
+      category: 'salads',
+      price: "5.99",
+    },
+    {
+      item: 'Cinnamon Roll',
+      category: 'dessert',
+      price: "8.99",
+    }
+  ];
 }
